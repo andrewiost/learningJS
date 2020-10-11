f3f8f1f4ee526459216e3570d65bf096d59ec041
andrewiost <andrewiost@ya.ru>
2020-10-11 23:14:26 +0700

    Выполнил задание для второго урока

--- script/script.js
+++ script/script.js
@@ -1,4 +1,22 @@
 let money, income, addExpenses, deposit, mission, period;
 
-alert('Lorem ipsum dolor sit amet, consectetur adipiscing elit');
-console.log('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+money = 30000;
+income = 'Фриланс';
+addExpenses = 'Интернет, такси, коммуналка';
+deposit = false;
+mission = 1000000;
+period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+let budgetDay = money/30;
+console.log(budgetDay);
