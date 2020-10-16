a63dff6e4f1e650eef1738f47952f337a0bbede5
andrewiost <andrewiost@ya.ru>
2020-10-17 02:13:12 +0700

    Выполнил 3 задание

--- script/script.js
+++ script/script.js
@@ -1,22 +1,42 @@
+'use strict';
+
 let money, income, addExpenses, deposit, mission, period;
 
-money = 30000;
+money = prompt('Ваш месячный доход?');
 income = 'Фриланс';
-addExpenses = 'Интернет, такси, коммуналка';
-deposit = false;
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
 mission = 1000000;
 period = 12;
 
+let expenses1 = prompt('Введите обязательную статью расходов', 'Еда'),
+    amount1 = prompt('Во сколько это обойдется?', '8000'), 
+    expenses2 = prompt('Введите обязательную статью расходов', 'Коммуналка'),
+    amount2 = prompt('Во сколько это обойдется?', '2500');
+
+let budgetMonth = Number(money) - (Number(amount1) + Number(amount2));
+let budgetDay = budgetMonth/30;
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+
+console.log(addExpenses);
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log('Бюджет на месяц с учетом расходов: ' + budgetMonth);
+console.log('Цель будет достигнута через: ' + (Math.ceil(mission/budgetMonth)) + ' месяцев');
+console.log('Бюджет на один день: ' + budgetDay);
 
-addExpenses = addExpenses.toLowerCase();
-addExpenses = addExpenses.split(', ');
-console.log(addExpenses);
-
-let budgetDay = money/30;
-console.log(budgetDay);
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 600) {
+    console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что-то пошло не так');
+}
